a39fde2c9de302377d7b1fba7c572df19c94750b
Felipe Bertoli <103579021+FelipeBertoli@users.noreply.github.com>
2023-02-22 12:16:44 -0300

    Update atividade.java

--- atividade.java
+++ atividade.java
@@ -1,15 +1,15 @@
 public class atividade {
-    public static void main(String[] args) {
+    public static void main(String[] args) {https://github.com/FelipeBertoli/Multiplica-Matrizes/blob/main/atividade.java
         int linhaA = 2;
-        int coluNaA = 3;
-        int linhaC = 2;
+        int colunaA = 3;
+        int linhaB = 2;
         int[][] matrizA = { {3, 1, 3}, {6, 5, 5} };
         int[][] matrizB = { {100, 50}, {50, 100}, {50, 50} };
 
-        int[][] matrizC = new int[linhaA][linhaC];
+        int[][] matrizC = new int[linhaA][linhaB];
         for(int i = 0; i < linhaA; i++) {
-            for (int j = 0; j < linhaC; j++) {
-                for (int k = 0; k < coluNaA; k++) {
+            for (int j = 0; j < linhaB; j++) {
+                for (int k = 0; k < colunaA; k++) {
                     matrizC[i][j] += matrizA[i][k] * matrizB[k][j];
                 }
             }
